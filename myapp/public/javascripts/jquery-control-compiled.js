a6bd161009abc16c03814672bfaa1f725e12b7e6
bll <bll@blldeMacBook-Pro.local>
2016-02-03 16:18:12 +0800

    localstorage存储信息

--- myapp/public/javascripts/jquery-control-compiled.js
+++ myapp/public/javascripts/jquery-control-compiled.js
@@ -3,31 +3,151 @@
  */
 var $ = require('jquery');
 var socket = io.connect('/', { res: 'id=123' });
-var $$;
 (function (jQuery, win, doc) {
-    "use strict";
-
+    //"use strict";
+    var okRequest = ['id', 'name'],
+        //符合的参数
+    searchRequest,
+        //请求参数字符串
+    $$,
+        //node节点
+    chatLocal,
+        //本地存贮
+    idAll = [],
+        //所有Id
+    existId = ['2'];
     $$ = {
         send: $("#send"),
+        main: $("#main > .body"),
         messageText: $("#message-text"),
         gotClone: $('#got-clone'),
         sentClone: $('#sent-clone'),
-        messageBody: $(".message>.body")
+        messageBody: $(".message>.body"),
+        empty: $("#empty"),
+        addPerson: $("#add-person")
     };
-    function getUser() {
-        var uid = window.location.search;
-        console.log(uid);
+    //聊天框,初始化
+    function init() {
+        chatLocal = localStorage.getItem('chat');
+        var search = window.location.search;
+        searchRequest = getUrlRequest(search.substr(1));
+        var getId = getUserId();
+        if (!searchRequest || !isSetId()) {
+            alert('id不存在或者参数不正确!!');
+            $('body').html("");
+            return false;
+        }
+        //getExistId();
     }
-    getUser();
+    //function getExistId(){
+    //
+    //
+    //}
+    //function setExistId(){
+    //
+    //    localStorage.setItem('id',);
+    //
+    //}
+    //判断是否存在该id
+    function isSetId() {
+        var chatArr = chatLocal.split(";");
+        var pass = false;
+        var isStr = '\"id\"' + ':\"' + getUserId() + '\"';
+        console.log(isStr);
+        chatArr.forEach(function (v, k) {
+            console.log(v, k);
+            if (v.indexOf(isStr) > -1) {
+                pass = true;
+            }
+        });
+        return pass;
+    }
+    //获取用户的id
+    function getUserId() {
+        var getId;
+        for (var k in searchRequest) {
+            if (searchRequest[k].indexOf('id') > -1) {
+                getId = searchRequest[k];
+                break;
+            }
+        }
+        return getId.split("=")[1];
+    }
+    //更新localstorage
+    function updateLocalChat(id, text, time) {
+        chatLocal = localStorage.getItem('chat');
+        var chat = { id: id, text: text, time: time };
+        var chatStr = JSON.stringify(chat);
+        var getLocal = chatLocal == null ? "" : chatLocal;
+        chatStr = getLocal + chatStr + ";";
+        localStorage.setItem('chat', chatStr);
+    }
+    //时间转化为时间戳
+    function timeToString(obj) {
+        var year = obj.getFullYear(),
+            //当前年份
+        month = obj.getMonth() + 1,
+            //当前月份
+        day = obj.getDate(),
+            //当前日
+        hour = obj.getHours(),
+            //获取当前小时数(0-23)
+        min = obj.getMinutes(),
+            //获取当前分钟数(0-59)
+        sec = obj.getSeconds(); //获取当前秒数(0-59)
+        return {
+            year: year, month: month, day: day, hour: hour, minute: min, second: sec
+        };
+    }
+    //获取合法参数
+    function getUrlRequest(search) {
+        var splitStr = search.indexOf('&') > -1 ? '&' : " ";
+        var sea = search.split(splitStr);
+        var pass = true;
+        for (var k in sea) {
+            var da = sea[k].split("=");
+            pass = filterRequest(da[0]);
+            if (!pass) break;
+        }
+        if (!pass) {
+            alert('参数不对');$('body').html("");return false;
+        }
+        return sea;
+    }
+    //过滤参数
+    function filterRequest(ke) {
+        var pass;
+        pass = $.inArray(ke, okRequest);
+        console.log(pass);
+        if (pass == -1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    //getUser();
     socket.on('connect', function () {
+        init();
         $$.send.on("click", function (e) {
             var text = $$.messageText.text();
+            var getId;
             if (text == "") return false;
             var sentClone = $$.sentClone.clone().removeClass("none");
             sentClone.find(".text").text(text);
+            //发送信息
             $$.messageBody.append(sentClone);
             $$.messageText.text("");
             socket.send(text);
+            var myDate = new Date();
+            var timeObj = timeToString(myDate);
+            var time = timeObj.year + "-" + timeObj.month + "-" + timeObj.day + " " + timeObj.hour + ":" + timeObj.minute + ":" + timeObj.second;
+            //获取id
+            var id = getUserId();
+            console.log(id);
+            updateLocalChat(id, text, time);
+        });
+        $$.addPerson.on("click", function () {
+            alert(1);
         });
     });
     $(document).on("keyup", function (event) {
@@ -42,6 +162,9 @@ var $$;
         var gotClone = $$.gotClone.clone().removeClass("none");
         gotClone.find(".text").text(data);
         $$.messageBody.append(gotClone);
+    });
+    $$.empty.on("click", function () {
+        $$.main.html("");
     });
 })($, window.document);
 
